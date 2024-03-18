fb722a7d94640746b8e3415de5722c2ac2a055a1
eigu <jmpheen@gmail.com>
2024-03-19 01:23:29 +0800

    Finished PlayerMovement, AudioManager, GameEventListener

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private List<Sound> _musicSounds;
+    [SerializeField] private List<Sound> _sfxSounds;
+
+    [SerializeField] private AudioSource _musicSource;
+    [SerializeField] private AudioSource _sfxSource;
+
+    public void PlayMusic(string name)
+    {
+        Sound sound = _musicSounds.Find(s => s.Name == name);
+
+        if (sound != null)
+        {
+            _musicSource.clip = sound.Clip;
+            _musicSource.Play();
+        }
+    }
+
+    public void PlaySFX(string names)
+    {
+        string[] nameArray = names.Split(',');
+
+        int randomIndex = Random.Range(0, nameArray.Length);
+        string randomName = nameArray[randomIndex].Trim();
+
+        Sound sound = _sfxSounds.Find(s => s.Name == randomName);
+
+        if (sound != null)
+        {
+            _sfxSource.PlayOneShot(sound.Clip);
+        }
+    }
+}
