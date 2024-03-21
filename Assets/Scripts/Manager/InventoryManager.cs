16ee984ee7c274dc726820e8a6683cd27ce0b543
Kyoko <108702599+KyoMatias@users.noreply.github.com>
2024-03-21 20:20:19 +0800

    Added Key

---
+++ Assets/Scripts/Manager/InventoryManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    [Header("Keys Attained")]
+    public bool HasSA_Key;
+    public bool HasKnob1;
+    public bool hasKnob2;
+    public bool hasKnob3;
+
+
+     
+    void AttainedSAKey(bool val)
+    {
+
+    }
+}
