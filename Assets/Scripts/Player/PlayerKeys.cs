306a8355c5d1e2da5372f67ff97cd4d6d37fc079
eigu <jmpheen@gmail.com>
2024-03-24 03:29:12 +0800

    -Added SceneManager to load next scene

--- Assets/Scripts/Player/PlayerKeys.cs
+++ Assets/Scripts/Player/PlayerKeys.cs
@@ -1,3 +1,4 @@
+using ScriptableObjectArchitecture;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,8 +6,18 @@ public class PlayerKeys : MonoBehaviour
 {
     [SerializeField] private List<string> _obtainedKeys = new List<string>();
 
+    [SerializeField] private GameEvent _onPlayerOpensLock;
+ 
     public void PickUpKey(string keyName)
     {
         _obtainedKeys.Add(keyName);
+    }
+
+    public void CheckForKey(string lockName)
+    {
+        if (_obtainedKeys.Contains(lockName))
+        {
+            _onPlayerOpensLock.Raise();
+        }
     }
 }
